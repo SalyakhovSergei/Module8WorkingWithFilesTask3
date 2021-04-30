3f05d0224f71e1ec64c63dfc7b352f030ef2a0f9
Sergey <ssm1988@inbox.ru>
2021-04-30 14:05:23 +1000

    Добавьте файлы проекта.

---
+++ Module8FilesTask3/FileDelete.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8FilesTask3
+{
+    public class FileActions
+    {
+        public void DeleteFiles(string folder)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folder);
+            DirectoryInfo[] directories = dirInfo.GetDirectories();
+            FileInfo[] FI = dirInfo.GetFiles();
+
+            if (dirInfo.Exists)
+            {
+                foreach (FileInfo file in FI)
+                {
+                    TimeSpan fileTime = DateTime.Now - file.CreationTime;
+                    if (fileTime.Minutes > 2)
+                    {
+                        file.Delete();
+                    }
+                }
+                foreach (DirectoryInfo DI in directories)
+                {
+                    DeleteFiles(DI.FullName);
+                }
+            }
+        }
+
+        public long FileSizeCount(string folder)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folder);
+            DirectoryInfo[] directories = dirInfo.GetDirectories();
+            FileInfo[] FI = dirInfo.GetFiles();
+            long allFileSize = 0;
+
+            if (dirInfo.Exists)
+            {
+                foreach (FileInfo file in FI)
+                {
+                    allFileSize += file.Length;
+                }
+                foreach (DirectoryInfo DI in directories)
+                {
+                    allFileSize += FileSizeCount(DI.FullName);
+                }
+            }
+            return allFileSize;
+        }
+    }
+}
+
---
+++ Module8FilesTask3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Module8FilesTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileActions test = new FileActions();
+            Console.WriteLine($"Общий размер папки: {test.FileSizeCount("C:/Users/Анатолий/Desktop/testFolder")}");
+            Console.WriteLine($"Очищено: {test.FileSizeCount("C:/Users/Анатолий/Desktop/testFolder")}");
+            test.DeleteFiles("C:/Users/Анатолий/Desktop/testFolder");
+            Console.WriteLine($"Текущий размер папки: {test.FileSizeCount("C:/Users/Анатолий/Desktop/testFolder")}");
+
+
+        }
+    }
+}
+
